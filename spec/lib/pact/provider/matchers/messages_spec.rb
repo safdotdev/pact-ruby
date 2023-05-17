f853d7b1ff9303db5c73659d483407779cede25f
Yousaf Nabi <you@saf.dev>
2023-05-17 19:39:29 +0100

    chore(test): update output_message_with_resets to use Rainbow

--- spec/lib/pact/provider/matchers/messages_spec.rb
+++ spec/lib/pact/provider/matchers/messages_spec.rb
@@ -12,7 +12,7 @@ module Pact
         let(:diff_formatter) { Pact::Matchers::UnixDiffFormatter }
         let(:message) { "line1\nline2"}
         let(:output_message) { "Actual: actual\n\n#{message}"}
-        let(:output_message_with_resets) { "Actual: \e[37mactual\e[0m\n\n#{Rainbow('line1').white}\n#{Rainbow('line2').white}"}
+        let(:output_message_with_resets) { "Actual: #{Rainbow('actual').white}\n\n#{Rainbow('line1').white}\n#{Rainbow('line2').white}"}
         let(:diff) { double("diff") }
         let(:actual) { "actual" }
         let(:color_enabled) { true }
