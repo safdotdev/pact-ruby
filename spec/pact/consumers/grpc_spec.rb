43c4420addf16a62dfff2c696cf097335a7a9699
Yousaf Nabi <yousafn@gmail.com>
2025-09-11 01:04:09 +0100

    test(v2): create mixed v4 provider verification

--- spec/pact/consumers/grpc_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "pact/v2/rspec"
-
-RSpec.describe "Pact::V2::Consumers::Grpc", :pact_v2 do
-  grpc_pact_provider "pact-ruby-v2-test-app"
-end
--- spec/pact/consumers/http_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "pact/v2/rspec"
-
-RSpec.describe "Pact::V2::Consumers::Http", :pact_v2 do
-  http_pact_provider "pact-ruby-v2-test-app"
-end
